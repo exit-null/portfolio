9fb433e46e33b9c148d3dc8e57da95afe5d22ce6
Sayan Banerjee <mail.sayanbanerjee99@gmail.com>
2021-07-21 18:02:33 +0530

    Addition of service worker and font awesome assets

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import { BaseProvider, LightTheme } from "baseui";
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from "./serviceWorker";
+import "./assests/font-awesome/css/all.css";
+
+const engine = new Styletron();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StyletronProvider value={engine}>
+    <BaseProvider theme={LightTheme}>
+      <App />
+    </BaseProvider>
+  </StyletronProvider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorker.unregister();
 reportWebVitals(console.log);
